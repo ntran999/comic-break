540055ba2fd269b09780c723ac760f72fa41aa2e
ntran999 <146213565+ntran999@users.noreply.github.com>
2023-12-05 21:13:54 +0000

    nav_bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:account_update, :keys => [:name])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :city, :comedian_bio, :comedian_name, :comedian_profile_pic, :email, :producer_bio, :producer_name, :producer_profile_pic, :short_comedian_bio, :state])
+  end
 end
